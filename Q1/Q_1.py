e18e48fbd7e51faa5a2990b970c36ded5a3e5bc9
Abdel Wahab <dbashayreh@gmail.com>
2025-03-13 13:05:28 +0100

    Question one

---
+++ Q1/Q_1.py
@@ -0,0 +1,15 @@
+"""
+
+Question: Create a program that does the following:
+
+Create two variables with numbers of your choice, add them together, and print the result.
+Perform subtraction, division, and multiplication with the same numbers, and print each result.
+
+CREATE  a file with the following naming convention 
+
+name_q1.py
+
+and push it under the folder Q1
+
+
+"""
