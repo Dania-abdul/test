b4593dd64adf11e927fbbb1b4376a86a27385e70
Lukas Sliva <lukas.sliva.developer@gmail.com>
2025-03-14 10:39:22 +0100

    first commit lukas branch

---
+++ Q1/lukas_q1.py
@@ -0,0 +1,7 @@
+num_one = 10
+num_two = 2
+
+print(f"{num_two} + {num_one} = {num_one + num_two}")
+print(f"{num_two} * {num_one} = {num_one * num_two}")
+print(f"{num_two} : {num_one} = {num_one / num_two}")
+print(f"{num_two} - {num_one} = {num_one - num_two}")
